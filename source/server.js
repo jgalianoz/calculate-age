270572cd8b5e0afc761870232ae3ba672d21896b
ManuelGalindez <manuelgalindez81@gmail.com>
2017-04-18 15:21:27 -0300

    [add] server render basic

---
+++ source/server.js
@@ -0,0 +1,24 @@
+import http from 'http';
+import React from 'react';
+import { renderToString, renderToStaticMarkup } from 'react-dom/server';
+
+import Layout from './Layout.jsx';
+
+function requestHandler(request, response) {
+
+  response.setHeader('Content-Type', 'text/html');
+
+  response.write(
+    renderToStaticMarkup(
+      <Layout
+        title="Calculate Age"
+      />
+    ),
+  );
+  response.end();
+}
+
+
+const server = http.createServer(requestHandler);
+
+server.listen(process.env.PORT || 3000);
